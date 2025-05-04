61625020b6f2cd66ff070dd3495a1eee6f269def
alt <163668836+amyhub-alt@users.noreply.github.com>
2025-05-04 12:36:56 -0400

    Use PostgreSQL on Heroku via dj_database_url

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -91,11 +91,9 @@ WSGI_APPLICATION = 'bookstore.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
 }
+
 
 
 # Password validation
